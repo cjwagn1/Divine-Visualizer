04f8922a850c4059e66755631ac457cf40ea8abf
Carter Wagner <cjwagn1@gmail.com>
2019-12-15 08:17:43 -0600

    fileSupport

--- src/components/main.js
+++ src/components/main.js
@@ -3,10 +3,26 @@ import React from "react";
 import "../css/main.css";
 
 export const Main = () => {
+  window.onload = function() {
+    var file = document.getElementById("input");
+    var audio = document.getElementById("audio");
+
+    file.onchange = function() {
+      var files = this.files;
+      audio.src = URL.createObjectURL(files[0]);
+      audio.load();
+      audio.play();
+    };
+  };
+
   return (
     <div className="center">
       <h1 className="font">Music Visualiztion</h1>
-      <button className="button">Upload</button>
+      <div>
+        <button className="button">Upload</button>
+        <input id="input" type="file" accept="audio/*" />
+        <audio id="audio" controls></audio>
+      </div>
     </div>
   );
 };
